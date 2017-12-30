d797b96901f4babff606dd76736e8fe8945a3d77
jbadan <jenna.badanowski@gmail.com>
2017-12-30 14:18:39 -0800

    component setup

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const PhotoGrid = React.createClass({
+  render(){
+    return(
+      <div>
+        photogrid
+      </div>
+    )
+  }
+});
+
+export default PhotoGrid;
