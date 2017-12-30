8f42163fe7ffe8f22de634db176fe261cc32fe8c
jbadan <jenna.badanowski@gmail.com>
2017-12-30 14:38:21 -0800

    config reducers

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,8 @@
+//reducer takes in 2 things- action - information about what happened and a copy of the currentstate
+
+
+function posts(state = [], action){
+  return state;
+}
+
+export default posts;
